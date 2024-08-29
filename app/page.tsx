74a4d5632bab7231f06783077762205ab25f7d45
Rafael Nóbrega de Lima <61426596+KylixXD@users.noreply.github.com>
2024-08-29 14:57:12 -0300

    Adicao prsma e tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import './globals.css'
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-      <h1>Hello World</h1>
+      <main>
+        <div className="bg-green-300">
+          <h1>TCC Search</h1>
+          <input type="text"></input>
+          <Link href="/create">Adicionar TCC</Link>
+        </div>
+      </main>
   );
 }
---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function UsersPage(){
+
+   return(
+    <main>
+        <h1>Teste de rota UsersPage</h1>
+        <Link href="/">Ir para Home</ Link>
+    </main>    
+   ); 
+}
